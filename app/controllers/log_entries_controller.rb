f19aa95e188bcfb6f6ab54b4bac73f24920f6157
William Manahan-Makley <info@willmakley.dev>
2021-10-31 08:16:09 -0400

    WIP log entries form and list

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -2,14 +2,66 @@
 
 class LogEntriesController < ApplicationController
   before_action :set_log
+  before_action :set_log_entry, only: [:show, :edit, :update, :destroy]
 
   def index
+    @log_entries = @log.log_entries.by_date_desc
 
+    @new_log_entry = LogEntry.new(log: @log)
+    if (most_recent_entry = @log_entries.first)
+      @new_log_entry.attributes = {
+        coffee: most_recent_entry.coffee,
+        water: most_recent_entry.water,
+        method: most_recent_entry.method,
+        grind: most_recent_entry.grind,
+        coffee_grams: most_recent_entry.coffee_grams,
+        water_grams: most_recent_entry.water_grams,
+      }
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @log_entry = LogEntry.new(log: @log)
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @log_entry.mark_as_deleted!
   end
 
   private
 
+    def log_entry_params
+      params.require(:log_entry)
+            .permit(
+              :coffee,
+              :water,
+              :method,
+              :grind,
+              :tasting,
+              :addl_notes,
+              :coffee_grams,
+              :water_grams
+            )
+    end
+
     def set_log
       @log = Log.find_by!(slug: params[:log_id])
+    end
+
+    def set_log_entry
+      @log_entry = @log.log_entries.find(params[:id])
     end
 end
