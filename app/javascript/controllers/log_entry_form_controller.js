544481b1ee5519c95c81728b61d6c8583a7a7c98
William Manahan-Makley <info@willmakley.dev>
2021-11-07 19:35:36 -0500

    clean up some basic turbo frames issues, WIP "other" brew method

--- app/javascript/controllers/log_entry_form_controller.js
+++ app/javascript/controllers/log_entry_form_controller.js
@@ -3,15 +3,33 @@ import { Controller } from "@hotwired/stimulus"
 export default class LogEntryFormController extends Controller {
   static targets = [
     "coffeeTextBox",
-    "submitButton"
+    "submitButton",
+    "brewMethodSelect",
+    "otherBrewMethod"
   ]
 
   connect() {
+    this.showOrHideOtherBrewMethod()
     this.enableOrDisableSubmit()
   }
 
   enableOrDisableSubmit() {
     const val = this.coffeeTextBoxTarget.value.trim()
-    this.submitButtonTarget.disabled = (val.length === 0)
+    this.submitButtonTarget.disabled = !val
+  }
+
+  showOrHideOtherBrewMethod() {
+    const brewMethod = this.brewMethodSelectTarget.value
+    if (brewMethod === "Other") {
+      this.otherBrewMethodTarget.disabled = false
+      this.otherBrewMethodTarget.style.display = ""
+      if (this.otherBrewMethodTarget.value === "") {
+        this.otherBrewMethodTarget.select()
+      }
+      // this.otherBrewMethodTarget.value = ""
+    } else {
+      this.otherBrewMethodTarget.style.display = "none"
+      this.otherBrewMethodTarget.disabled = true
+    }
   }
 }
