26259d2fed3a372b0190d957399841403f6d8af6
William Manahan-Makley <info@willmakley.dev>
2021-11-10 19:49:26 -0500

    add ability to switch between logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include IpBanningConcern
   include HttpBasicAuthentication
+
+  before_action :set_logs
+
+  private
+
+  def set_logs
+    @logs = Log.all.load
+  end
 end
