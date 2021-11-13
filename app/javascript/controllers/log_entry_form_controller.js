c1e8ed196ff735f19e75bf9f789f3e84c5ffa0a7
William Manahan-Makley <info@willmakley.dev>
2021-11-12 20:55:58 -0500

    Add clear button to make it easier to create a new entry with a blank slate

--- app/javascript/controllers/log_entry_form_controller.js
+++ app/javascript/controllers/log_entry_form_controller.js
@@ -2,12 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class LogEntryFormController extends Controller {
   static targets = [
+    "brewMethodSelect",
     "coffeeTextBox",
     "submitButton",
   ]
 
   connect() {
     this.enableOrDisableSubmit()
+  }
+
+  clear() {
+    this.element.querySelectorAll('input[type="text"]').forEach((input) => {
+      input.value = ""
+    })
+    this.element.querySelector("textarea").value = ""
+    this.brewMethodSelectTarget.selectedIndex = 0
   }
 
   enableOrDisableSubmit() {
