e4cf10e35c18676a20b82ad3d342e83c185a7408
William Manahan-Makley <info@willmakley.dev>
2021-11-13 20:34:50 -0500

    add https redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   include IpBanningConcern
   include HttpBasicAuthentication
 
+  before_action :redirect_to_https, unless: -> { request.ssl? || request.local? }
   before_action :set_logs
 
   private
 
-  def set_logs
-    @logs = Log.all
-  end
+    def redirect_to_https
+      redirect_to protocol: "https://"
+    end
+
+    def set_logs
+      @logs = Log.all
+    end
 end
