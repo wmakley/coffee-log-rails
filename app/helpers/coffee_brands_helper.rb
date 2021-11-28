42459be2027759428a4b5b9d215b7d289accd301
William Manahan-Makley <info@willmakley.dev>
2021-11-28 08:58:46 -0500

    improve display of coffee brands

--- app/helpers/coffee_brands_helper.rb
+++ app/helpers/coffee_brands_helper.rb
@@ -23,9 +23,17 @@ module CoffeeBrandsHelper
 
   def coffee_brand_logo_card_top(coffee_brand, html_options = {})
     if coffee_brand.logo.attached?
-      image_tag(coffee_brand.logo,
+      image_tag(coffee_brand.logo.variant(resize_to_fill: [400, 200]),
                 html_options.reverse_merge(alt: "#{coffee_brand.name} Logo",
                                            class: "card-img-top"))
+    end
+  end
+
+  def coffee_brand_logo_card_side(coffee_brand, html_options = {})
+    if coffee_brand.logo.attached?
+      image_tag(coffee_brand.logo.variant(resize_to_fill: [400, 500]),
+                html_options.reverse_merge(alt: "#{coffee_brand.name} Logo",
+                                           class: "img-fluid rounded-start"))
     end
   end
 end
