729b7d5da491fddcdbaacdf563ee69dd5ac04327
William Manahan-Makley <info@willmakley.dev>
2021-11-28 11:05:15 -0500

    implement coffee form CRUD

--- app/controllers/coffee_photos_controller.rb
+++ app/controllers/coffee_photos_controller.rb
@@ -18,11 +18,17 @@ class CoffeePhotosController < ApplicationController
       format.html do
         redirect_to edit_coffee_url(@coffee), notice: "Successfully deleted photo."
       end
-      format.turbo_stream
+      format.turbo_stream do
+        set_coffee_brand_options
+      end
     end
   end
 
   private
+
+    def set_coffee_brand_options
+      @coffee_brand_options = CoffeeBrand.for_select
+    end
 
     def set_coffee
       @coffee = Coffee.find(params[:coffee_id])
--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -8,26 +8,36 @@ class CoffeesController < ApplicationController
   end
 
   def show
-    @coffee = Coffee.find(params[:id])
-
     @log_entries = @coffee.log_entries.by_date_desc.includes(:log)
   end
 
   def new
     @coffee = Coffee.new
-    @coffee_brand_options = CoffeeBrand.for_select
+    set_coffee_brand_options
   end
 
   def create
     @coffee = Coffee.new(coffee_params)
+
+    if @coffee.save
+      redirect_to @coffee, notice: "Successfully created coffee."
+    else
+      set_coffee_brand_options
+      render action: :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-    @coffee = Coffee.find(params[:id])
+    set_coffee_brand_options
   end
 
   def update
-    @coffee = Coffee.find(params[:id])
+    if @coffee.update(coffee_params)
+      redirect_to @coffee, notice: "Successfully updated coffee."
+    else
+      set_coffee_brand_options
+      render action: :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -42,7 +52,11 @@ class CoffeesController < ApplicationController
   private
 
     def set_coffee
-      @coffeee = Coffee.find(params[:id])
+      @coffee = Coffee.find(params[:id])
+    end
+
+    def set_coffee_brand_options
+      @coffee_brand_options = CoffeeBrand.for_select
     end
 
     def coffee_params
