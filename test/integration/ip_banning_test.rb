16f7661e13c2722a7d1ef3bc69eda37f455dddf4
William Manahan-Makley <info@willmakley.dev>
2022-03-09 12:49:06 -0500

    add integration tests for password reset fail2ban logic

--- test/integration/ip_banning_test.rb
+++ test/integration/ip_banning_test.rb
@@ -5,7 +5,9 @@ require 'test_helper'
 class IpBanningTest < ActionDispatch::IntegrationTest
   fixtures :logs, :users
 
-  test "IPs are banned after 10 failed attempts" do
+  test "IPs are banned after 10 failed login attempts" do
+    assert_not Fail2Ban.banned?('127.0.0.1')
+
     invalid_params = {
       login_form: {
         username: random_string(16),
@@ -19,6 +21,48 @@ class IpBanningTest < ActionDispatch::IntegrationTest
     end
 
     post "/session", params: invalid_params
+    assert_response :not_found
+
+    assert Fail2Ban.banned?('127.0.0.1')
+  end
+
+  test "IPs are banned after 10 failed password reset requests" do
+    assert_not Fail2Ban.banned?('127.0.0.1')
+
+    invalid_params = {
+      password_reset_request: {
+        email: "test@asdfasdfasf.com",
+      }
+    }
+
+    Fail2Ban::MAX_ATTEMPTS.times do |n|
+      post "/password_reset_request", params: invalid_params
+      assert_redirected_to "/", "#{n}th iteration"
+    end
+
+    post "/password_reset_request", params: invalid_params
+    assert_response :not_found
+
+    assert Fail2Ban.banned?('127.0.0.1')
+  end
+
+  test "IPs are banned after 10 failed password reset attempts" do
+    assert_not Fail2Ban.banned?('127.0.0.1')
+
+    invalid_params = {
+      password_reset: {
+        token: '1234',
+        password: 'asdfasdf',
+        password_confirmation: 'asdfasdf'
+      }
+    }
+
+    Fail2Ban::MAX_ATTEMPTS.times do |n|
+      patch "/password", params: invalid_params
+      assert_response :unprocessable_entity, "#{n}th iteration"
+    end
+
+    patch "/password", params: invalid_params
     assert_response :not_found
 
     assert Fail2Ban.banned?('127.0.0.1')
