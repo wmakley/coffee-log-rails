3435e15af091c104368b25534ee3476066511b68
William Manahan-Makley <info@willmakley.dev>
2022-07-23 21:14:12 -0400

    debug password resets

--- app/mailers/password_reset_request_mailer.rb
+++ app/mailers/password_reset_request_mailer.rb
@@ -2,6 +2,8 @@
 
 class PasswordResetRequestMailer < ApplicationMailer
   def reset_password_link
+    puts params.inspect
+    raise ArgumentError, "user is nil" if params[:user].nil?
     @user = params[:user]
     mail(to: @user.email, subject: "Reset Coffee Log Password")
   end
