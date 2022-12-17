f458f0094b9fbf66afa6d85a9c947dcf701d80d2
William Manahan-Makley <info@willmakley.dev>
2022-12-17 08:42:30 -0500

    fix fail2ban unit test

--- test/models/fail2_ban_test.rb
+++ test/models/fail2_ban_test.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+require "test_helper"
+
 class Fail2BanTest < ActiveSupport::TestCase
   test "#record_failed_attempt" do
     ip = '1.2.3.4'
 
     record = Fail2Ban.record_failed_attempt(ip)
+    assert record.is_a?(LoginAttempt)
     assert record.persisted?
     assert_equal 1, record.attempts
 
     record = Fail2Ban.record_failed_attempt(ip)
+    assert record.is_a?(LoginAttempt)
     assert_equal 2, record.attempts
 
     assert_not Fail2Ban.banned?(ip)
