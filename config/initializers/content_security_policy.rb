8e89a866b0c745b6096a61003d2fc53b6886b046
William Manahan-Makley <info@willmakley.dev>
2023-08-18 21:05:49 -0400

    tighten up CSP, apparently google handles it with nonces somehow

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,13 +6,13 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    policy.default_src :self, :https, 'https://cdn.jsdelivr.net'
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
+    policy.default_src :self, 'https://cdn.jsdelivr.net'
+    policy.font_src    :self, :data
+    policy.img_src     :self, :data
     policy.object_src  :none
-    policy.script_src  :self, :https, 'inline', 'https://www.google.com/recaptcha/', 'https://www.gstatic.com/recaptcha/', 'https://www.recaptcha.net/'
-    policy.frame_src   :self, :https, 'https://www.google.com/recaptcha/', 'https://recaptcha.google.com/recaptcha/'
-    policy.style_src   :self, :https, "'unsafe-inline'"
+    policy.script_src  :self, :inline
+    policy.frame_src   :self
+    policy.style_src   :self, "'unsafe-inline'"
     # Specify URI for violation reports
     policy.report_uri "https://fdl3vgy26hvmtjgl3hxhvshe4a0aerqf.lambda-url.us-east-1.on.aws/"
   end
