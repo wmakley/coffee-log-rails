2207bacf66b2ccd8ef1fdd1c942348eeb8a9d7a4
William Manahan-Makley <info@willmakley.dev>
2023-08-20 13:53:36 -0400

    Recaptcha: simplify helpers, add enterprise project id

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,3 @@
+module SessionsHelper
+  include Recaptcha::Adapters::ViewMethods
+end
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,6 @@
 Recaptcha.configure do |config|
   config.enterprise = true
   config.site_key   = ENV['RECAPTCHA_SITE_KEY'] || Rails.application.credentials.dig(Rails.env.to_sym, :recaptcha, :site_key)
+  config.enterprise_project_id = ENV['RECAPTCHA_ENTERPRISE_PROJECT_ID'] || Rails.application.credentials.dig(Rails.env.to_sym, :recaptcha, :enterprise_project_id)
   config.enterprise_api_key = ENV['RECAPTCHA_ENTERPRISE_API_KEY'] || Rails.application.credentials.dig(Rails.env.to_sym, :recaptcha, :enterprise_api_key)
 end
