e6b42c9c18230c5491a2188c0ab5a7014abf3bad
William Manahan-Makley <info@willmakley.dev>
2023-08-20 13:33:40 -0400

    put back the full CSP suite (google's docs cannot be relied upon)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,12 +7,12 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, 'https://cdn.jsdelivr.net'
-    policy.font_src    :self, :data
-    policy.img_src     :self, :data
+    policy.font_src    :self, :data, 'https://cdn.jsdelivr.net'
+    policy.img_src     :self, :data, 'https://cdn.jsdelivr.net'
     policy.object_src  :none
-    policy.script_src  :self
-    policy.frame_src   :self
-    policy.style_src   :self, "'unsafe-inline'"
+    policy.script_src  :self, 'https://cdn.jsdelivr.net', 'https://www.google.com/recaptcha/', 'https://www.gstatic.com/recaptcha/', 'https://www.recaptcha.net/recaptcha/'
+    policy.frame_src   :self, 'https://www.google.com/recaptcha/', 'https://recaptcha.google.com/recaptcha/', 'https://www.recaptcha.net/recaptcha/'
+    policy.style_src   :self, "'unsafe-inline'", 'https://cdn.jsdelivr.net'
     # Specify URI for violation reports
     policy.report_uri "https://fdl3vgy26hvmtjgl3hxhvshe4a0aerqf.lambda-url.us-east-1.on.aws/"
   end
