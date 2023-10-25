dcc7ddfde9f702a5002a36cf5e4d3a7ebdc574ab
William Manahan-Makley <info@willmakley.dev>
2023-10-25 15:27:52 -0400

    fix autoloading issues with lib directory being covered by zeitwerk

--- config/initializers/sesv2_delivery_method.rb
+++ config/initializers/sesv2_delivery_method.rb
@@ -1,6 +1,6 @@
-require_relative "../../lib/sesv2_delivery_method"
+require_relative "../../lib/delivery_methods/sesv2"
 
-ActionMailer::Base.add_delivery_method :ses, Sesv2DeliveryMethod,
+ActionMailer::Base.add_delivery_method :ses, DeliveryMethods::Sesv2,
   region: ENV["AWS_REGION"] || Rails.application.credentials.dig(Rails.env.to_sym, :aws, :region),
   credentials: Aws::Credentials.new(
     ENV["AWS_ACCESS_KEY_ID"] || Rails.application.credentials.dig(Rails.env.to_sym, :aws, :access_key_id),
