42a5253d2848e47ed6fdb5d0dcd2ffa295bc7a8b
William Manahan-Makley <info@willmakley.dev>
2023-12-03 15:30:37 -0500

    login form error handling clarification

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -18,24 +18,35 @@ module Auth
     end
 
     def create
-      success = verify_recaptcha(action: "login", minimum_score: 0.5)
-      logger.info "Recaptcha success: #{success}"
-      if !success
+      @login_form = LoginForm.new(login_form_params)
+
+      # missing required fields
+      if @login_form.invalid?
+        return render action: :new, status: :unprocessable_entity
+      end
+
+      # captcha failure
+      recaptcha_result = verify_recaptcha(action: "login", minimum_score: 0.5)
+      logger.info "Recaptcha success: #{recaptcha_result}"
+      unless recaptcha_result
         logger.warn "Recaptcha reply is nil" if recaptcha_reply.nil?
         score = recaptcha_reply["score"] if recaptcha_reply
         logger.info("User was denied login because of a recaptcha score of #{score.inspect} | reply: #{recaptcha_reply.inspect}")
+
+        flash[:error] = "ReCAPTCHA verification failed."
+        return render action: :new, status: :unprocessable_entity
       end
 
-      @login_form = LoginForm.new(login_form_params)
-      return_to = session[:return_to]
-      if success && authenticate_user_from_form(@login_form)
-        url = return_to.presence || logs_url
-        redirect_to url, status: :see_other
-      else
+      # username / password failure
+      unless authenticate_user_from_form(@login_form)
         flash[:error] = "Username or password not correct."
         Fail2Ban.record_failed_attempt(request.remote_ip) if @login_form.valid?
-        render action: :new, status: :unprocessable_entity
+        return render action: :new, status: :unprocessable_entity
       end
+
+      # success
+      url = session[:return_to].presence || logs_url
+      redirect_to url, status: :see_other
     end
 
     def destroy
