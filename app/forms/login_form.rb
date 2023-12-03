a52aeeb76777ffca7e8d28879ad0aa1f38d6b957
William Manahan-Makley <info@willmakley.dev>
2023-12-03 15:27:58 -0500

    fix typo, less of rails' weird error style

--- app/forms/login_form.rb
+++ app/forms/login_form.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# Validates form inputs structurually. No business logic.
+# Validates form inputs structurally. No business logic.
 class LoginForm
   include ActiveModel::Model
 
   attr_accessor :username, :password
 
-  validates_presence_of :username, :password
+  validates_presence_of :username, :password, message: "%{attribute} is required"
 
   def persisted?
     false
