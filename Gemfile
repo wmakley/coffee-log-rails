8e4421a9a0ec3d39310d7ee02779a0e8b17ef260
William Manahan-Makley <will@willmakley.dev>
2026-05-17 14:35:24 -0400

    generate inertia install

--- Gemfile
+++ Gemfile
@@ -119,3 +119,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 # gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "inertia_rails", "~> 3.21"
+
+gem "vite_rails", "~> 3.11"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
+  constraints(host: "127.0.0.1") do
+    get "(*path)", to: redirect { |params, req| "#{req.protocol}localhost:#{req.port}/#{params[:path]}" }
+  end
+  get 'inertia-example', to: 'inertia_example#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
