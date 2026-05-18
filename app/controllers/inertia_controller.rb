4e184fb1918ad599abac0c4e6a72cf44b6652d61
William Manahan-Makley <will@willmakley.dev>
2026-05-17 20:49:02 -0400

    create inertia layout and revert most changes to OG layout

--- app/controllers/inertia_controller.rb
+++ app/controllers/inertia_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class InertiaController < ApplicationController
+  layout "inertia"
+
   # Share data with all Inertia responses
   # see https://inertia-rails.dev/guide/shared-data
   #   inertia_share user: -> { Current.user&.as_json(only: [:id, :name, :email]) }
