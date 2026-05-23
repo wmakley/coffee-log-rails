9d6073a5300465c315a06bdb92e992d4a2d27f8a
William Manahan-Makley <will@willmakley.dev>
2026-05-23 13:53:30 -0400

    rails 8.1: filter usernames out as well

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -6,5 +6,5 @@
 Rails.application.config.filter_parameters += [
   :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc,
   # custom:
-  :email_verification_token, :password_digest, :reset_password_token
+  :email_verification_token, :password_digest, :reset_password_token, :username
 ]
